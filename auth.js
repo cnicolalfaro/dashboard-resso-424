4d6382edecb25040c85285a863d5705a27997525
Nami <c.nicol.alfaro@gmail.com>
2026-09-22 19:27:27 -0300

    filtro y error barra

--- auth.js
+++ auth.js
@@ -57,7 +57,7 @@
     s.src = "app.js?v=7";
     s.onload = function () {
       var incidents = document.createElement("script");
-      incidents.src = "incidentes-v2.js?v=7";
+      incidents.src = "incidentes-v2.js?v=8";
       document.body.appendChild(incidents);
     };
     document.body.appendChild(s);
